e5beee60a138a7274ffc420f5767bc5da41ce645
m3nininho <luizmenino22@gmail.com>
2022-12-09 18:11:19 -0300

    added the accordion

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const img = '../assets/arrow-down.svg'
 const linkDefault = 'https://apiv3.apifootball.com/?action=get_teams&league_id=302&APIkey=';
 const apiKey = '14b47eef4be63e8d7d01b52ac8291f127c01de4011aac559448da4f88d7c8060';
 const getTeams = linkDefault + apiKey;
@@ -20,18 +21,36 @@ function collectedData(data){
 
   teams.forEach((team) => {
     const box = document.createElement('div');
-
-    box.classList.add('box-team');
+    const infos = document.createElement('div');
     teamsBox.appendChild(box);
-
+    box.classList.add('box-team');
     box.innerHTML = 
     `
     <img src=${team.team_badge}>
     <p>${team.team_name}</p>
-    <img src=${'../assets/arrow-down.svg'} class="arrow">
+    <img src=${img} class="arrow">
+    `   
+    teamsBox.appendChild(infos)
+    infos.classList.add('box-infos');
+    infos.innerHTML = `
+      <p>Coach: ${team.coaches[0].coach_name}</p>
     `
-   
   })
 
+  const clubs = document.querySelectorAll('.box-team');
+  const infos = document.querySelectorAll('.box-infos');
+  const arrow = document.querySelectorAll('.arrow')
+
+  for(let i = 0; i < infos.length; i++){
+    clubs[i].addEventListener('click', function (){
+      infos[i].classList.toggle('active');
+      infos[i].classList.toggle('box-infos');
+      arrow[i].classList.toggle('arrow-active');
+    })
+  }
+
+
 }
+
+
 
